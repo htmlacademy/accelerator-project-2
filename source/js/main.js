2adcea67ffc8df9ebe46e628381afff6957b6b91
restby <restby@gmail.com>
2024-08-09 22:34:41 +0300

    tours, tours-slide, fix btn section-title, new tours images

--- source/js/main.js
+++ source/js/main.js
@@ -1,10 +1,11 @@
 import { openMenu } from '../js/modules/open-menu';
 import { heroSliderInit } from '../js/modules/sliders/hero-slider';
+import { toursSliderInit } from '../js/modules/sliders/tours-slider';
 
 window.addEventListener('DOMContentLoaded', () => {
   openMenu();
   heroSliderInit();
   window.addEventListener('load', () => {
-
+    toursSliderInit();
   });
 });

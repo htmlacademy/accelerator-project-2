c1bbc0f9fbaf03d9a971149e32e02e800ff565f4
anna <anka-kudrow@mail.ru>
2024-08-29 15:47:39 +0300

    removed all !important from promo section

--- source/js/header-nav.js
+++ source/js/header-nav.js
@@ -7,17 +7,17 @@ const bodyEl = document.querySelector(".page-body");
 const menuList = bodyEl.querySelector(".header-nav__list");
 
 function closeMenu() {
-  headerOpen.classList.remove("burger-button--closed");
-  headerClose.classList.add("burger-button--closed");
+  headerOpen.classList.remove("burger-button-close--closed");
+  headerClose.classList.add("burger-button-close--closed");
   headerMenu.classList.add("header-nav__wrapper---closed");
   fadedPromoBlock.style.filter = "none";
   bodyEl.style.overflow = "auto";
 }
 
 headerBtn.addEventListener("click", () => {
-  if (!headerOpen.classList.contains("burger-button--closed")) {
-    headerOpen.classList.add("burger-button--closed");
-    headerClose.classList.remove("burger-button--closed");
+  if (!headerOpen.classList.contains("burger-button-close--closed")) {
+    headerOpen.classList.add("burger-button-close--closed");
+    headerClose.classList.remove("burger-button-close--closed");
     headerMenu.classList.remove("header-nav__wrapper---closed");
     fadedPromoBlock.style.filter = "brightness(0.5)";
     bodyEl.style.overflow = "hidden";

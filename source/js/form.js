f475815aebe8f5c1b3ad6d5cb70e813a2e578337
Лиана <verbovskayaliana@gmail.com>
2024-08-30 17:44:35 +0300

    тесты проходят все

--- source/js/form.js
+++ source/js/form.js
@@ -1,78 +1,35 @@
-// function validate(){
-//   var form = document.querySelector("form__input--email");
-//   var email = document.getElementById("email").value;
-//   var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/
-
-//   if(email.match(pattern))
-//   {
-//       form.classList.add("valid");
-//       form.classList.remove("invalid");
-//   }
-//   else{
-//       form.classList.remove("valid");
-//       form.classList.add("invalid");
-//   }
-//   if (email == "") {
-//       form.classList.remove("valid");
-//       form.classList.remove("invalid");
-//   }
-// }
-
-
-// document.addEventListener( "DOMContentLoaded" , function() {
-// document.querySelector(".input").addEventListener("input", function() {
-//   this.classList.toggle("invalid", !this.validity.valid);
-// })
-// });
-
-
-const nameInput = document.querySelector('#phone');
-const formItemName = document.querySelector('.form__input--email');
-const formItemPhone = document.querySelector('.form__input--phone');
-const phoneInput = document.querySelector('#email');
-const form = document.querySelector('.form__position');
+const phoneInput = document.querySelector('#phone');
+const emailInput = document.querySelector('#email');
+const sectionForm = document.querySelector('.form__position');
 const formButton = document.querySelector('.form__position .form__button');
 
-function validateName() {
-  const namePattern = /\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/;
-  if (!namePattern.test(nameInput.value)) {
-    formItemPhone.classList.add('error');
-    // insertTextNotification(formItemPhone, 'Пожалуйста, введите только номер телефона');
+function validatePhone() {
+  const phonePattern = /^\+\d+$/;
+  if (!phonePattern.test(phoneInput.value)) {
+    phoneInput.classList.add('error');
     return false;
   }
-  nameInput.classList.remove('error');
+  phoneInput.classList.remove('error');
   return true;
 }
 
-// function validatePhone() {
-//   const phonePattern = /^([0-9a-zA-Z]+[-._+&])*[0-9a-zA-Z]+@([-0-9a-zA-Z]+[.])+[a-zA-Z]{2,6}$/;
-//   if (!phonePattern.test(phoneInput.value)) {
-//     phoneInput.classList.add('error');
-//     insertTextNotification(formItemName, 'Пожалуйста, введите только email');
-//     return false;
-//   }
-//   phoneInput.classList.remove('error');
-//   return true;
-// }
+function validateEmail() {
+  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!emailPattern.test(emailInput.value)) {
+    emailInput.classList.add('error');
+    return false;
+  }
+  emailInput.classList.remove('error');
+  return true;
+}
 
-// function insertTextNotification(el, text) {
-//   const existingNotification = el.querySelector('p');
-//   if (existingNotification) {
-//     existingNotification.remove();
-//   }
-
-//   const notitficationElement = document.createElement('p');
-//   notitficationElement.textContent = text;
-//   el.appendChild(notitficationElement);
-// }
-
-export function vaildateForm() {
+export function validateForm() {
 
   formButton.addEventListener('click', (e) => {
     e.preventDefault();
-    const isValid = validateName() && validatePhone();
+    const isValid = validatePhone() && validateEmail();
     if (isValid) {
-      form.submit();
+      sectionForm.submit();
     }
   });
 }
--- source/js/main.js
+++ source/js/main.js
@@ -1,9 +1,3 @@
-// https://swiperjs.com/get-started#installation
-// import Swiper from "swiper";
-// import {Navigation, Pagination} from "swiper/modules";
-// import 'swiper/css';
-
-
 import './menu';
 import './hero';
 import './tours';
